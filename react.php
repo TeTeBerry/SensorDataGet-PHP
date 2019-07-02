acf87028d6c2d4c0a1bc86f6e26ced19af7d95ef
TeTeBerry <250714751@qq.com>
2019-07-03 02:43:59 +0700

    realtime api for react

--- react.php
+++ react.php
@@ -1,3 +1,50 @@
+<script type="text/javascript">
+
+    var url = window.location.search.substr(1);
+    console.log(url);
+   function QueryStringToJSON(str) {
+    var pairs = str.split('&');
+    var result = {};
+    pairs.forEach(function (pair) {
+        pair = pair.split('=');
+        var name = pair[0]
+        var value = pair[1]
+        if (name.length)
+            if (result[name] !== undefined) {
+                if (!result[name].push) {
+                    result[name] = [result[name]];
+                }
+                result[name].push(value || '');
+            } else {
+                result[name] = value || '';
+            }
+    });
+    return (result);
+}
+
+var obj = QueryStringToJSON(url);
+
+console.log(obj)
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                var response = xhr.responseText;
+                console.log(response)
+            }
+        }
+    };
+
+    xhr.open('POST','http://127.0.0.1:8088/react.php', true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    xhr.send(obj);
+    console.log(obj);
+
+
+</script>
+
 <?php
 ob_start();
 include('post-esp.data.php');
