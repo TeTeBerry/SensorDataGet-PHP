04db2e0447ba4f24d096efbab5626a8cd7865a86
TeTeBerry <250714751@qq.com>
2019-07-15 04:01:41 +0700

    deploy

--- index.php
+++ index.php
@@ -1,25 +1,20 @@
 <?php
 
-
+require('vendor/autoload.php');
 header("Access-Control-Allow-Origin: *");
-
 $servername = "127.0.0.1";
-
 // REPLACE with your Database name
 $dbname = "iot";
 // REPLACE with Database user
 $username = "root";
 // REPLACE with Database user password
 $password = "root";
-
-
 if (!empty($_GET["sensorName"])) {
         // Calls from sensor to save data
         $sensorName = $_GET["sensorName"];
         $flowRate = $_GET["flowRate"];
         $flowMilliLitres = $_GET["flowMilliLitres"];
         $totalMilliLitres = $_GET["totalMilliLitres"];
-
         $meter = array("sensorName"=>$_GET["sensorName"], "flowRate"=>$_GET["flowRate"],"flowMilliLitres"=>$_GET["flowMilliLitres"], "totalMilliLitres"=>$_GET["totalMilliLitres"]);
       
         // Create connection
@@ -28,17 +23,12 @@ if (!empty($_GET["sensorName"])) {
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-
          $sql2 = "INSERT INTO realTime (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
             VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
-
              $result2 = $conn->query($sql2);
-
-
         $filterSql = "SELECT * FROM sensorData WHERE totalMilliLitres = '".$_GET["totalMilliLitres"]."'";
         $filerQuery = mysqli_query($conn,$filterSql);
         $filterResult = mysqli_fetch_array($filerQuery);
-
         if($filterResult) {
             echo "totalMilliLitres already exist";
         }
@@ -49,7 +39,6 @@ if (!empty($_GET["sensorName"])) {
             
             $result = $conn->query($sql);
            
-
             if ($result&&$result2 === TRUE) {
                 echo "New records created successfully";
             }
@@ -60,18 +49,14 @@ if (!empty($_GET["sensorName"])) {
         }
     
         $conn->close();
-
-
-
     
 }else{
     echo "API not called with HTTP GET method";
 }
-
-
-function test_input($caller) {
-    $caller = trim($caller);
-    $caller = stripslashes($caller);
-    $caller = htmlspecialchars($caller);
-    return $caller;
-}
+// function test_input($caller) {
+//     $caller = trim($caller);
+//     $caller = stripslashes($caller);
+//     $caller = htmlspecialchars($caller);
+//     return $caller;
+// }
+?>
