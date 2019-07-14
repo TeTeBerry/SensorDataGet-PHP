4c372f5ce38f57495dd949dd88c80b242d5334e9
TeTeBerry <250714751@qq.com>
2019-07-15 05:06:12 +0700

    file

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/' :
-        require __DIR__ . '/index.php';
+        require __FILE__ . '/index.php';
         break;
     case '/realtime' :
-        require __DIR__ . '/react.php';
+        require __FILE__ . '/react.php';
         break;
     default:
-        require __DIR__ . '/404.php';
+        require __FILE__ . '/404.php';
         break;
 }
