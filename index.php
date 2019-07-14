48612a9f32cc54832522ddd07659c6a6f73aff69
TeTeBerry <250714751@qq.com>
2019-07-15 05:04:34 +0700

    deploy

--- index.php
+++ index.php
@@ -1,64 +1,16 @@
 <?php
-
 require('vendor/autoload.php');
-header("Access-Control-Allow-Origin: *");
-$servername = "us-cdbr-iron-east-02.cleardb.net";
 
-// REPLACE with your Database name
-$dbname = "heroku_0abf4b67ee6747a";
-// REPLACE with Database user
-$username = "b5b7819d9b19a5";
-// REPLACE with Database user password
-$password = "47532b88";
+$request = $_SERVER['REQUEST_URI'];
 
-if (!empty($_GET["sensorName"])) {
-        // Calls from sensor to save data
-        $sensorName = $_GET["sensorName"];
-        $flowRate = $_GET["flowRate"];
-        $flowMilliLitres = $_GET["flowMilliLitres"];
-        $totalMilliLitres = $_GET["totalMilliLitres"];
-        $meter = array("sensorName"=>$_GET["sensorName"], "flowRate"=>$_GET["flowRate"],"flowMilliLitres"=>$_GET["flowMilliLitres"], "totalMilliLitres"=>$_GET["totalMilliLitres"]);
-      
-        // Create connection
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        //Check connection
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-         $sql2 = "INSERT INTO realTime (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
-            VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
-             $result2 = $conn->query($sql2);
-        $filterSql = "SELECT * FROM sensorData WHERE totalMilliLitres = '".$_GET["totalMilliLitres"]."'";
-        $filerQuery = mysqli_query($conn,$filterSql);
-        $filterResult = mysqli_fetch_array($filerQuery);
-        if($filterResult) {
-            echo "totalMilliLitres already exist";
-        }
-        else{
-        
-            $sql = "INSERT INTO sensorData (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
-            VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
-            
-            $result = $conn->query($sql);
-           
-            if ($result&&$result2 === TRUE) {
-                echo "New records created successfully";
-            }
-        
-            else {
-                echo "Error: " . $sql . "<br>" . $conn->error;
-            }
-        }
-    
-        $conn->close();
-    
-}else{
-    echo "API not called with HTTP GET method";
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/index.php';
+        break;
+    case '/realtime' :
+        require __DIR__ . '/react.php';
+        break;
+    default:
+        require __DIR__ . '/404.php';
+        break;
 }
-// function test_input($caller) {
-//     $caller = trim($caller);
-//     $caller = stripslashes($caller);
-//     $caller = htmlspecialchars($caller);
-//     return $caller;
-// }
-?>
