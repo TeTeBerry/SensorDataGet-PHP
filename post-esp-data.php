a007f283533a83a04deaca3239e053ef9fc174c6
TeTeBerry <250714751@qq.com>
2019-07-15 04:01:34 +0700

    deploy

--- post-esp-data.php
+++ post-esp-data.php
@@ -1,6 +1,8 @@
 <?php
 
 
+header("Access-Control-Allow-Origin: *");
+
 $servername = "127.0.0.1";
 
 // REPLACE with your Database name
@@ -10,77 +12,66 @@ $username = "root";
 // REPLACE with Database user password
 $password = "root";
 
-$data_value = "sensorData";
-$client_value = "react";
 
+if (!empty($_GET["sensorName"])) {
+        // Calls from sensor to save data
+        $sensorName = $_GET["sensorName"];
+        $flowRate = $_GET["flowRate"];
+        $flowMilliLitres = $_GET["flowMilliLitres"];
+        $totalMilliLitres = $_GET["totalMilliLitres"];
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $data = test_input($_GET["data"]);
-    if($data == $data_value) {
-        $sensorName = test_input($_GET["sensorName"]);
-        $flowRate = test_input($_GET["flowRate"]);
-        $flowMilliLitres = test_input($_GET["flowMilliLitres"]);
-        $totalMilliLitres = test_input($_GET["totalMilliLitres"]);
-
-        $meter= array("sensorName"=>$_GET["sensorName"], "flowRate"=>$_GET["flowRate"],"flowMilliLitres"=>$_GET["flowMilliLitres"], "totalMilliLitres"=>$_GET["totalMilliLitres"]);
-
- 
+        $meter = array("sensorName"=>$_GET["sensorName"], "flowRate"=>$_GET["flowRate"],"flowMilliLitres"=>$_GET["flowMilliLitres"], "totalMilliLitres"=>$_GET["totalMilliLitres"]);
+      
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
-        // Check connection
+        //Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-        } 
+        }
+
+         $sql2 = "INSERT INTO realTime (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
+            VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
+
+             $result2 = $conn->query($sql2);
 
 
-        $filterSql = "SELECT * FROM SensorData WHERE totalMilliLitres = '".$_GET["totalMilliLitres"]."'";
+        $filterSql = "SELECT * FROM sensorData WHERE totalMilliLitres = '".$_GET["totalMilliLitres"]."'";
         $filerQuery = mysqli_query($conn,$filterSql);
         $filterResult = mysqli_fetch_array($filerQuery);
+
         if($filterResult) {
             echo "totalMilliLitres already exist";
         }
         else{
         
-        $sql = "INSERT INTO SensorData (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
-        VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
+            $sql = "INSERT INTO sensorData (sensorName, flowRate, flowMilliLitres, totalMilliLitres)
+            VALUES ('" . $sensorName . "', '" . $flowRate . "', '" . $flowMilliLitres . "', '" . $totalMilliLitres . "')";
+            
+            $result = $conn->query($sql);
+           
+
+            if ($result&&$result2 === TRUE) {
+                echo "New records created successfully";
+            }
         
-        $result = $conn->query($sql);
-
-
-
-
-        if ($result === TRUE) {
-              echo json_encode($meter);
-
-}
-        
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+            else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
         }
-    }
     
         $conn->close();
 
-}
+
+
     
-
-} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $client = $_GET["client"];
-    if($client == $client_value) {
-
-        echo "string";
-
-    }
-}
-
-else {
-    echo "No data posted with HTTP GET.";
+}else{
+    echo "API not called with HTTP GET method";
 }
 
 
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+function test_input($caller) {
+    $caller = trim($caller);
+    $caller = stripslashes($caller);
+    $caller = htmlspecialchars($caller);
+    return $caller;
 }
--- react.php
+++ react.php
@@ -1,7 +1,55 @@
 <?php
-ob_start();
-include('post-esp.data.php');
-ob_clean();
+
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+// (1) INIT
+set_time_limit(60); // Set the appropriate time limit
+ignore_user_abort(false); // Stop when polling breaks
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'iot');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
  
-echo $meter;
+// (2) DATABASE
+class DB {
+  protected $pdo = null;
+  protected $stmt = null;
+  function __construct() {
+    try {
+      $this->pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, 
+        DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false]
+      );
+      return true;
+    } catch (Exception $ex) {
+      print_r($ex);
+      die();
+    }
+  }
+  function __destruct() {
+    if ($this->stmt !== null) { $this->stmt = null; }
+    if ($this->pdo !== null) { $this->pdo = null; }
+  }
+  function getData() {
+    $this->stmt = $this->pdo->prepare("SELECT * FROM `realTime` ORDER BY `reading_time` DESC LIMIT 1");
+    $this->stmt->execute();
+    $result = $this->stmt->fetchAll();
+    return count($result)==0 ? ["sensorName"=>0, "flowRate"=>0, "flowMilliLitres"=>0, "totalMilliLitres"=>0,"reading_time"=>0] : ["reading_time"=>strtotime($result[0]['reading_time']), "sensorName"=>$result[0]['sensorName'], "flowRate"=>$result[0]['flowRate'],"flowMilliLitres"=>$result[0]['flowMilliLitres'],"totalMilliLitres"=>$result[0]['totalMilliLitres']];
+  }
+}
+$pdoDB = new DB();
+
+// (3) LOOP - CHECK FOR UPDATES
+// Will keep looping until a score update or AJAX timeout
+while (true) {
+  $data = $pdoDB->getData();
+  if ($data['reading_time']>$_GET['last']) {
+    echo json_encode($data);
+    break;
+  }
+  sleep(1);
+}
 ?>
